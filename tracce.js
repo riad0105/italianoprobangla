fb75e5274c67dddab4d2cc1959fdaa0662ea54d6
riad0105 <6wnnz94kbd@privaterelay.appleid.com>
2026-07-26 18:28:59 +0200

    Update tracce.js

--- tracce.js
+++ tracce.js
@@ -1,4 +1,4 @@
 const tracce = [
-  { unita: "Ciao", pagina: "Pagina 1-3", titolo: "Alfabeto italiano", file: "ALFABETO.MP3" },
-  { unita: "Ciao", pagina: "Pagina 4", titolo: "Presentarsi", file: "PRESENTARSI.MP3" },
+  { id: "pagina1-3", unita: "Ciao", pagina: "Pagina 1-3", titolo: "Alfabeto italiano", file: "ALFABETO.MP3" },
+  { id: "pagina4", unita: "Ciao", pagina: "Pagina 4", titolo: "Presentarsi", file: "PRESENTARSI.MP3" },
 ];
