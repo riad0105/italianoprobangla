bdf714db36d8fc6d448a2ece88e390087c31d0ba
riad0105 <6wnnz94kbd@privaterelay.appleid.com>
2026-07-26 18:19:17 +0200

    Update tracce.js

--- tracce.js
+++ tracce.js
@@ -1,3 +1,4 @@
 const tracce = [
   { unita: "Ciao", pagina: "Pagina 1-3", titolo: "Alfabeto italiano", file: "ALFABETO.MP3" },
+  { unita: "Ciao", pagina: "Pagina 4", titolo: "Presentarsi", file: "PRESENTARSI.MP3" },
 ];
